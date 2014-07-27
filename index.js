514590189bbc8d099c7fe721dd70c9d1688a0390
Yury Korolev <yurykorolev@me.com>
2014-07-27 13:59:45 +0400

    Example

---
+++ index.js
@@ -0,0 +1,29 @@
+var Hapi = require('hapi');
+
+var serverOptions = {
+  views: {
+    engines: {
+      'slm': require('slm')
+    },
+    basePath: __dirname,
+    path: './views',
+    compileOptions: {
+      basePath: __dirname + '/views'
+    },
+    isCached: true
+  }
+}
+
+var server = new Hapi.Server(3000, serverOptions);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.view('index', {hello: "word"});
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
